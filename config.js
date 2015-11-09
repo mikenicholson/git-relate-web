e8b049ea7923695904d40d10c6c48881b38312a7
Mike Nicholson <themikenicholson@gmail.com>
2015-11-08 20:35:12 -0500

    Initial checkin
    Just a simple api that returns true or false

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    repo_dir: "/Users/mike/Projects/passport-jwt",
+};
+
+module.exports = config
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+    bodyparser = require('body-parser'),
+    config = require('./config'),
+    child_process = require('child_process'),
+    util = require('util'),
+    morgan = require('morgan');
+
+var app = express();
+
+app.use(bodyparser.json());
+
+app.post('/api/isnewer', function (req, res) {
+    ancestor = req.body.ancestor;
+    descendant = req.body.descendant;
+
+    opts = { cwd: config.repo_dir };
+    cmd = util.format("git merge-base --is-ancestor %s %s", ancestor, descendant)
+    child_process.exec(cmd, opts, function(err, stdout, stderr) {
+        var result; 
+        if (err) {
+            result = false;
+        } else {
+            result = true; 
+        }
+
+        res.send({result: result}); 
+    });
+
+});
+
+
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Is newer app listening at http://%s:%s', host, port);
+});
